df02872cf1d8f7126167f8f253f31eddd485dced
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2022-11-01 12:51:30 -0400

    Update __init__.py

--- initials/__init__.py
+++ initials/__init__.py
@@ -15,22 +15,22 @@ def compiles():
 @check50.check(compiles)
 def uppercase():
     """Outputs HLJ for Hailey Lynn James"""
-    check50.run("./initials").stdin("Hailey Lynn James", prompt=False).stdout(match("HLJ"), "HLJ\n").exit(0)
+    check50.run("./initials").stdin("Hailey Lynn James", prompt=False).stdout(match("Initials: HLJ"), "HLJ\n").exit(0)
 
 @check50.check(compiles)
 def lowercase():
     """Outputs HLJ for hailey lynn james"""
-    check50.run("./initials").stdin("hailey lynn james", prompt=False).stdout(match("HLJ"), "HLJ\n").exit(0)
+    check50.run("./initials").stdin("hailey lynn james", prompt=False).stdout(match("Initials: HLJ"), "HLJ\n").exit(0)
 
 @check50.check(compiles)
 def mixed_case():
     """Outputs HJ for hailey James"""
-    check50.run("./initials").stdin("hailey James", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+    check50.run("./initials").stdin("hailey James", prompt=False).stdout(match(Initials: "HJ"), "HJ\n").exit(0)
 
 @check50.check(compiles)
 def all_uppercase():
     """Outputs B for BRIAN"""
-    check50.run("./initials").stdin("BRIAN", prompt=False).stdout(match("B"), "B\n").exit(0)
+    check50.run("./initials").stdin("BRIAN", prompt=False).stdout(match("Initials: B"), "B\n").exit(0)
 
 def match(initials):
     return "^(.*\n)?{}\n".format(initials)
