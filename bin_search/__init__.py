378b84b6f1ec70722670c262ab5d43d1dc5202a2
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2022-11-01 15:30:21 -0400

    Create __init__.py

---
+++ bin_search/__init__.py
@@ -0,0 +1,32 @@
+import check50
+import check50.c
+
+
+@check50.check()
+def exists():
+    """binary.c exists"""
+     check50.exists("binary.c")
+
+      
+@check50.check(exists)
+def compiles():
+    """binary.c compiles"""
+    check50.c.compile("binary.c", lcs50=True)
+             
+             
+@check50.check(compiles)
+def finds_2():
+    """binary search finds 2"""
+    check50.run("./binary").stdin("2").stdout("Found\n").exit(0)
+
+    
+@check("compiles")
+def finds_14():
+    """binary search finds 14"""
+    check50.run("./binary").stdin("14").stdout("Found\n").exit(0)
+
+    
+@check("compiles")
+def finds_9():
+    """binary search does not fine 9"""
+    check50.run("./binary").stdin("9").stdout("Not found!\n").exit(0)
