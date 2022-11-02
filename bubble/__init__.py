ee89b86ffc0df813c8ef766a7edd2d14031650d6
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2022-11-02 12:52:07 -0400

    Update __init__.py

--- bubble/__init__.py
+++ bubble/__init__.py
@@ -11,10 +11,10 @@ def exists(self):
 @check50.check(exists)
 def compiles(self):
     """bubble.c compiles."""
-     check50.c.compile("bubble.c", lcs50=True).exit(0)
+     check50.c.compile("bubble.c", lcs50=True)
 
         
 @check50.check(compiles)
 def sorts():
-    """sorts {64, 34, 25, 12, 22, 11, 90}"""
-    check50.run("./bubble").stdout("11 12 22 25 34 64 90 \n").exit(0)
+    """sorts {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}"""
+    check50.run("./bubble").stdout("0 1 2 3 4 5 6 7 8 9 \n").exit(0)
