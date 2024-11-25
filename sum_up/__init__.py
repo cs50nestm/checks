c3224d8573116931470540d5d41fc0dcec25428e
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2024-11-25 17:42:18 -0500

    Update __init__.py

--- sum_up/__init__.py
+++ sum_up/__init__.py
@@ -23,6 +23,6 @@ def sums_to_5():
 @check50.check(compiles)
 def sums_to_10():
     """sumup sums to 10"""
-    check50.run("./sumup").stdin("19").stdout("The sum from 1 to 10 is 55").exit(0)
+    check50.run("./sumup").stdin("10").stdout("The sum from 1 to 10 is 55").exit(0)
 
   
