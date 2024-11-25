239983cebde5023b248859f006f252b6b17f0f4b
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2024-11-25 17:32:43 -0500

    Create __init__.py

---
+++ sum_up/__init__.py
@@ -0,0 +1,28 @@
+import check50
+import check50.c
+
+
+@check50.check()
+def exists():
+    """sumup.c exists"""
+    check50.exists("sumup.c")
+
+      
+@check50.check(exists)
+def compiles():
+    """sumup.c compiles"""
+    check50.c.compile("sumup.c", lcs50=True)
+             
+             
+@check50.check(compiles)
+def sums_to_5():
+    """sumup sums to 5"""
+    check50.run("./sumup").stdin("5").stdout("15").exit(0)
+
+    
+@check50.check(compiles)
+def sums_to_10():
+    """sumup sums to 10"""
+    check50.run("./sumup").stdin("19").stdout("55").exit(0)
+
+  
