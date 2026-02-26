4ddfdc3a8459975faa88ac372d5d62fbf2f17ec2
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2026-02-26 10:23:04 -0500

    Refactor check function to submitted for homepage

--- homepage/__init__.py
+++ homepage/__init__.py
@@ -1,9 +1,5 @@
 import check50
-import check50.c
-import re
-
 
 @check50.check()
-def exists(self):
-    """homepagereflect.txt exists."""
-    check50.exists("homepagereflect.txt")
+def submitted():
+    """Homepage submitted"""
